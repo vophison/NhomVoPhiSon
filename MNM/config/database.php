4baf0ffdc35264fe905cd52e89932601b1a93845
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2020-12-30 09:42:41 +0700

    luu lai database

--- MNM/config/database.php
+++ MNM/config/database.php
@@ -1,9 +1,9 @@
 <?php
  header("Content-type: text/html; charset=utf-8");
  $tenmaychu='localhost';
- $tentaikhoan='id11411442_tuongvu123';
- $pass='N64!Q!tCpo_AV0To';
- $csdl='id11411442_tuongvu';
+ $tentaikhoan='root';
+ $pass='';
+ $csdl='naruto';
  $conn=mysqli_connect($tenmaychu, $tentaikhoan, $pass, $csdl);
  mysqli_select_db($conn,$csdl);
  mysqli_query($conn,"SET NAMES 'UTF8'");
